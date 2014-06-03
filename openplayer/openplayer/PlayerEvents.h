020c6be9cad83d85631e24f8812327758174e0e1
radu.motisan <radhoo.tech@gmail.com>
2014-06-03 10:24:02 +0300

    misc

---
+++ openplayer/openplayer/PlayerEvents.h
@@ -0,0 +1,13 @@
+//
+//  PlayerEvents.h
+//  openplayer
+//
+//  Created by Radu Motisan on 03/06/14.
+//  Copyright (c) 2014 AudioNowDigital. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlayerEvents : NSObject
+
+@end
---
+++ openplayer/openplayer/PlayerEvents.m
@@ -0,0 +1,13 @@
+//
+//  PlayerEvents.m
+//  openplayer
+//
+//  Created by Radu Motisan on 03/06/14.
+//  Copyright (c) 2014 AudioNowDigital. All rights reserved.
+//
+
+#import "PlayerEvents.h"
+
+@implementation PlayerEvents
+
+@end
