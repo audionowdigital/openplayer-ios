f309d0a0b9bb873c31cd31471878b3c87af5627c
radu.motisan <radhoo.tech@gmail.com>
2014-06-04 19:21:06 +0300

    added vorbis and removed redundant code

---
+++ openplayer/libs/libvorbis/VorbisDecoder.h
@@ -0,0 +1,18 @@
+//
+//  OpusDecoder.m
+//  libopus
+//
+//  Created by Radu Motisan on 03/06/14.
+//  Copyright (c) 2014 AudioNowDigital. All rights reserved.
+//
+
+
+#import "INativeInterface.h"
+
+
+/*
+ * Starts the decoding from a opus bitstream to pcm
+ */
+int vorbisDecodeLoop(id<INativeInterface> callback);
+
+
