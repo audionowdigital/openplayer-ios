e5bafc36a5249ef5f5bb9e7f657f9a8655577579
Catalin BORA <catalin.bora@audionowdigital.com>
2014-06-12 17:35:16 +0300

    stream connection work .. still has bugs.

--- openplayer/openplayerdemo/MainViewController.m
+++ openplayer/openplayerdemo/MainViewController.m
@@ -45,13 +45,13 @@
     
     NSString *urlString =
 //    @"http://ai-radio.org:8000/radio.opus"; //stereo ok
-//    @"http://www.markosoft.ro/opus/02_Archangel.opus"
+    @"http://www.markosoft.ro/opus/02_Archangel.opus";
 //    @"http://www.markosoft.ro/opus/countdown.opus"
     
    // @"http://repeater.xiph.org:8000/temporalfugue.opus";//mono stream!!
  //   @"http://repeater.xiph.org:8000/clock.opus"; //stereo ok
 //    @"http://revolutionradio.ru:8000/live.ogg"; //vorbis
-    @"http://ice01.va.audionow.com:8000/radioamericaopus.ogg"; //stereo opus ok.
+//    @"http://ice01.va.audionow.com:8000/radioamericaopus.ogg"; //stereo opus ok.
 //    @"http://icecast.timlradio.co.uk/ar64.opus"; // not ok
 //    @"http://icecast.timlradio.co.uk/ac96.opus"; //not ok
     //@"http://opus.ai-radio.org:8000/radio.opus";
