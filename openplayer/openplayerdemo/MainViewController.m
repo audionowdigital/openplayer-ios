8a44f2eb8823145082f96d17fe9bc87841c6b1d4
Florin Moisa <florin.moisa@audionowdigital.com>
2014-06-25 13:12:42 +0300

    stop open player when an error comes

--- openplayer/openplayerdemo/MainViewController.m
+++ openplayer/openplayerdemo/MainViewController.m
@@ -73,8 +73,8 @@
     //http://radioserver1.delfa.net:80/256.opus";
     //http://ogg.ai-radio.org:8000/radio.ogg";
     
-    @"http://icecast1.pulsradio.com:80/mxHD.ogg";
-//    @"http://www.markosoft.ro/opus/02_Archangel.opus";
+//    @"http://icecast1.pulsradio.com:80/mxHD.ogg";
+    @"http://www.markosoft.ro/opus/02_Archangel.opus";
 //    file_path;
    
     NSString *url2String =
